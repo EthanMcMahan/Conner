207c5439268734923b591d81793b2799643e65cb
Ethan McMahan <cowboyfanet@hotmail.com>
2023-01-23 16:24:35 -0800

    added html, css, and js for birthday confetti.

--- conner.js
+++ conner.js
@@ -26,3 +26,67 @@ const changeColor = () => {
 };
 
 button.addEventListener('click', changeColor)
+
+
+for (var i = 0; i < 250; i++) {
+    create(i);
+  }
+  
+  function create(i) {
+    var width = Math.random() * 8;
+    var height = width * 0.4;
+    var colourIdx = Math.ceil(Math.random() * 3);
+    var colour = "red";
+    switch (colourIdx) {
+      case 1:
+        colour = "yellow";
+        break;
+      case 2:
+        colour = "blue";
+        break;
+      default:
+        colour = "red";
+    }
+    $('<div class="confetti-' + i + ' ' + colour + '"></div>').css({
+      "width": width + "px",
+      "height": height + "px",
+      "top": -Math.random() * 20 + "%",
+      "left": Math.random() * 100 + "%",
+      "opacity": Math.random() + 0.5,
+      "transform": "rotate(" + Math.random() * 360 + "deg)"
+    }).appendTo('#birthdays');
+  
+    drop(i);
+  }
+  
+  function drop(x) {
+    $('.confetti-' + x).animate({
+      top: "100%",
+      left: "+=" + Math.random() * 15 + "%"
+    }, Math.random() * 3000 + 3000, function() {
+      reset(x);
+    });
+  }
+  
+  function reset(x) {
+    $('.confetti-' + x).animate({
+      "top": -Math.random() * 20 + "%",
+      "left": "-=" + Math.random() * 15 + "%"
+    }, 0, function() {
+      drop(x);
+    });
+  }
+  
+  var $bd = $('#birthdays');
+  $('.close-bd').on('click touchstart', function() {
+    
+    $bd.hide()
+    // localStorage.setItem('birthdays', true);  //set flag   - does not work at Stackoverflow
+    $bd.find("[class^=confetti]").stop(true, true).fadeOut("slow");
+  });
+  
+  // var show = localStorage.getItem('birthdays')
+  show = true; // remove after uncommenting 
+  $bd.toggle(show);
+
+  
